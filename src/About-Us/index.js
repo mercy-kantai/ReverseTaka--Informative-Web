6bb1c4ada3caed4ea95928d347a133876f847e54
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-06-16 22:39:29 +0300

    add file

---
+++ src/About-Us/index.js
@@ -0,0 +1,36 @@
+import'./index.css'
+const About = ()=>{
+    return(
+        <div>
+            <div>
+                <p> We are Reverse Taka, our mission is to reduce waste and promote environmental responsibility <br></br>by providing a platform for customers to purchase high quality recycled clothes.
+The damage caused<br></br>by fast fashion to the environment is a big fact, so textile waste is. The textile waste increases by billions <br></br>of tons every year, and the environment is under great threat because of this. There are some sustainable <br></br>methods that everyone can easily use to prevent waste generation and environmental damage, and recycling <br></br>is one of them</p>
+            </div>
+            <div className='icons'>
+            <div>
+                <figure>
+                    <img src='https://img.icons8.com/?size=50&id=2965&format=png' alt='recycling'></img>
+                    <figcaption> RECYCLING</figcaption>
+                </figure>
+            </div>
+            <div>
+                <figure>
+                    <img src='https://img.icons8.com/?size=50&id=2282&format=png' alt='environment'></img>
+                    <figcaption>ENVIRONMENTAL</figcaption>
+                </figure>
+            </div>
+            
+            <div>
+                <figure>
+                    <img src='https://img.icons8.com/?size=48&id=nASQLOHpf52c&format=png' alt='platform'></img>
+                    <figcaption> PLATFORM</figcaption>
+                </figure>
+            </div>
+            </div>
+
+        </div>    
+);
+}
+
+
+export default About;
