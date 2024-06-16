9b4c1ca96cd13698e8aaa5319ffea4ba44d09ada
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-06-16 22:31:52 +0300

    Creates footer

---
+++ src/Contacts/index.js
@@ -0,0 +1,29 @@
+import './index.css'
+
+function Contact(){
+    return(
+        <div className='footer'>
+            <h3>Contact Us On</h3>
+            <div className='contactInfo'>
+            <div className='telNo'>
+                <p>+254 793 244 456</p>
+                <p>+254 786 965 766</p>
+                <p>+254 742 052 402</p>
+                <p>+254 704 264 110</p>
+                <p>+254 712 345 678</p>
+            </div>
+            <div className='location'>
+                <p>Unit no. 75 ReverseTaka Industry Nairobi</p>
+            </div>
+            <div className='emails'>
+                <p>akwangdeu@gmail.com</p>
+                <p>natalywambui@gmail.com</p>
+                <p>kantaimercy@gmail.com</p>
+                <p>ivywanjiku@gmail.com</p>
+                <p>maureenmwendwa@gmail.com</p>
+            </div>
+            </div>
+        </div>
+    );
+}
+export default Contact;
