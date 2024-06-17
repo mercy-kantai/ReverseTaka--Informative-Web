9986d937d0bc5edba39033bd06bde59e38b970a1
Maureen-Gatweri <maureennmwendwa@gmail.com>
2024-06-17 10:53:32 +0300

    Add icons on about page

--- src/About-Us/index.js
+++ src/About-Us/index.js
@@ -1,28 +1,33 @@
+
 import'./index.css'
+import { FaRecycle } from "react-icons/fa6";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { BsMenuApp } from "react-icons/bs";
 const About = ()=>{
     return(
         <div>
             <div>
+                <h3> About us</h3>
                 <p> We are Reverse Taka, our mission is to reduce waste and promote environmental responsibility <br></br>by providing a platform for customers to purchase high quality recycled clothes.
 The damage caused<br></br>by fast fashion to the environment is a big fact, so textile waste is. The textile waste increases by billions <br></br>of tons every year, and the environment is under great threat because of this. There are some sustainable <br></br>methods that everyone can easily use to prevent waste generation and environmental damage, and recycling <br></br>is one of them</p>
             </div>
             <div className='icons'>
             <div>
                 <figure>
-                    <img src='https://img.icons8.com/?size=50&id=2965&format=png' alt='recycling'></img>
+                    <p className='img'><FaRecycle size='8.5rem'/></p>
                     <figcaption> RECYCLING</figcaption>
                 </figure>
             </div>
             <div>
                 <figure>
-                    <img src='https://img.icons8.com/?size=50&id=2282&format=png' alt='environment'></img>
+                    <p className='img'><FaHandHoldingHeart size='8.5rem'/></p>
                     <figcaption>ENVIRONMENTAL</figcaption>
                 </figure>
             </div>
             
             <div>
                 <figure>
-                    <img src='https://img.icons8.com/?size=48&id=nASQLOHpf52c&format=png' alt='platform'></img>
+                    <p className='img'> <BsMenuApp size='8.5rem'/></p>
                     <figcaption> PLATFORM</figcaption>
                 </figure>
             </div>
